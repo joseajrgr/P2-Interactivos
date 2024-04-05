04d8936fd80e9bd32679335bdadab5e99a8132f4
joseajgj <quincemarzo4@gmail.com>
2024-04-05 10:29:48 +0200

    voz

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function agregarAlCarrito() {
-    var productoInput = document.getElementById("productoInput");
-    var producto = productoInput.value.trim();
+function agregarAlCarrito(producto) {
+    if (producto === undefined) {
+        var productoInput = document.getElementById("productoInput");
+        var producto = productoInput.value.trim();
+    }
+    console.log('Agregando al carrito: ' + producto);
+    
     if (producto !== "") {
         var carrito = document.getElementById("carrito");
         var li = document.createElement("li");
@@ -9,3 +13,29 @@ function agregarAlCarrito() {
         productoInput.value = "";
     }
 }
+
+// Función para iniciar el reconocimiento de voz
+function iniciarReconocimientoVoz() {
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+        var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        recognition.lang = 'es-ES'; // Configura el idioma a español
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+
+        recognition.onresult = function(event) {
+            var speechResult = event.results[0][0].transcript;
+            console.log('Resultado de reconocimiento de voz: ' + speechResult);
+            agregarAlCarrito(speechResult);
+        };
+
+        recognition.onerror = function(event) {
+            console.log('Error de reconocimiento de voz: ' + event.error);
+        };
+
+        recognition.start();
+    } else {
+        alert('Tu navegador no soporta reconocimiento de voz.');
+    }
+}
+// Añadir un event listener al botón de microfono
+document.getElementById("microfonoBtn").addEventListener("click", iniciarReconocimientoVoz);
